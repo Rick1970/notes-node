86f5ee0b95d17722fb97504471e5917f58b9bb8f
Rick Thornbrugh <Rick Thornbrugh>
2017-02-03 15:15:02 -0800

    refactor read function.

--- app.js
+++ app.js
@@ -10,33 +10,39 @@ const argv = yargs.argv;
 var command = argv._[0];
 console.log('Command: ', command);
 console.log('Yargs', argv);
+//*****************************************************************
+
 if(command === 'add') {
   var note = notes.addNote(argv.title, argv.body);
   if(note){
     console.log("Note created.");
-    console.log('--');
-    console.log(`Title: ${note.title}`);
-    console.log(`Body: ${note.body}`);
+    notes.logNote(note);
   } else {
     console.log('Note title taken.');
   }
+//******************************************************************
 } else if(command === 'list') {
   notes.getAll();
+
+
+//******************************************************************
 } else if (command === 'read') {
-  var returnedNote = notes.getNote(argv.title);
-  if(returnedNote){
+  var note = notes.getNote(argv.title);
+  if(note){
     console.log("Note found");
-    console.log('--');
-    console.log(`Title: ${returnedNote.title}`);
-    console.log(`Body: ${returnedNote.body}`);
+    notes.logNote(note);
   } else {
     console.log('No note matching title found.');
   }
+//*******************************************************************
+
 
 } else if (command === 'remove') {
   var noteRemoved = notes.removeNote(argv.title);
   var message = noteRemoved ? 'Note was removed.' : 'Note not found.';
   console.log(message);
+//*********************************************************************
+
 } else {
   console.log('The command is not recognized.');
 }
